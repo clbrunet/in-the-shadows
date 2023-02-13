e7b96d4dedc85ba5b1db08e70ba34fdcdec85a0c
clbrunet <brunet.clement5@gmail.com>
2023-02-13 11:20:33 +0100

    feat(LevelSelection): add a guard to the level selectors
    If the mouse is moved between a mouse down and up then it is ignored

--- Assets/Scripts/LevelSelection/LevelSelector.cs
+++ Assets/Scripts/LevelSelection/LevelSelector.cs
@@ -7,9 +7,22 @@ public class LevelSelector : MonoBehaviour
 {
     [SerializeField]
     private LevelDataSO levelData;
+    private Vector2 mouseDownPosition;
 
     private void OnMouseDown()
     {
+        mouseDownPosition = Input.mousePosition;
+    }
+
+    private void OnMouseUp()
+    {
+        Vector2 delta = (Vector2)Input.mousePosition - mouseDownPosition;
+        delta.x = Mathf.Abs(delta.x);
+        delta.y = Mathf.Abs(delta.y);
+        if (delta.x + delta.y > 10)
+        {
+            return;
+        }
         LevelManager.levelData = levelData;
         SceneManager.LoadScene("Level");
     }
